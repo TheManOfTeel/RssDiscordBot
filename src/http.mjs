02105d83b117d9677ce691d12cff9c9011a822af
Danny Teel <themanofteel@icloud.com>
2026-08-26 22:03:58 -0400

    Add missing headers

--- src/http.mjs
+++ src/http.mjs
@@ -34,8 +34,9 @@ export function describeError(err) {
 export async function fetchFeed(url, { etag, lastModified, timeoutMs = 20_000, retries = 2, fetchImpl = fetch, log = () => {} } = {}) {
   const headers = {
     accept: 'application/rss+xml, application/atom+xml, application/xml, text/xml, */*;q=0.5',
-    'accept-encoding': 'gzip, deflate, br, utf-8',
+    'accept-encoding': 'gzip, deflate, br',
     'user-agent': USER_AGENT,
+    'access-control-allow-origin': '*',
   };
   if (etag) headers['if-none-match'] = etag;
   if (lastModified) headers['if-modified-since'] = lastModified;
